721df406ba3d3609977e28f7223b1cd216349606
Badjau <franzvasquez4153@gmail.com>
2025-08-07 20:09:33 +0800

    modifiers.c: fixed

--- boards/shields/nice_oled/widgets/modifiers.c
+++ boards/shields/nice_oled/widgets/modifiers.c
@@ -3,18 +3,15 @@
  *
  * SPDX-License-Identifier: MIT
  */
-
 #include <zephyr/bluetooth/services/bas.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
-
 #include <dt-bindings/zmk/modifiers.h>
 #include <zmk/display.h>
 #include <zmk/event_manager.h>
 #include <zmk/events/keycode_state_changed.h>
 #include <zmk/hid.h>
-
 #include "modifiers.h"
 
 struct modifiers_state {
@@ -22,43 +19,48 @@ struct modifiers_state {
 };
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
-
 static lv_obj_t *modifier_label = NULL;
 
 static void set_modifiers_text(lv_obj_t *label,
                                struct modifiers_state ignored) {
   uint8_t mods = zmk_hid_get_explicit_mods();
-
+  
   if (modifier_label) {
     lv_obj_del(modifier_label);
     modifier_label = NULL;
   }
-
+  
   if (mods == 0) {
     return;
   }
-
+  
   modifier_label = lv_label_create(label);
   lv_obj_center(modifier_label);
   
-  char mod_text[20] = {0};
+  char mod_text[32] = {0}; // Increased buffer size for multiple modifiers
+  bool first = true;
   
+  // Check each modifier and append with proper delimiter
   if (mods & (MOD_LGUI | MOD_RGUI)) {
     strcat(mod_text, "GUI");
+    first = false;
   }
   if (mods & (MOD_LALT | MOD_RALT)) {
-    if (mod_text[0] != '\0') strcat(mod_text, "+");
+    if (!first) strcat(mod_text, "+");
     strcat(mod_text, "ALT");
+    first = false;
   }
   if (mods & (MOD_LCTL | MOD_RCTL)) {
-    if (mod_text[0] != '\0') strcat(mod_text, "+");
-    strcat(mod_text, "CTL");
+    if (!first) strcat(mod_text, "+");
+    strcat(mod_text, "CTRL");
+    first = false;
   }
   if (mods & (MOD_LSFT | MOD_RSFT)) {
-    if (mod_text[0] != '\0') strcat(mod_text, "+");
-    strcat(mod_text, "SFT");
+    if (!first) strcat(mod_text, "+");
+    strcat(mod_text, "SHFT");
+    first = false;
   }
-
+  
   lv_label_set_text(modifier_label, mod_text);
   lv_obj_align(modifier_label, LV_ALIGN_TOP_LEFT, 36, 0);
 }
@@ -81,6 +83,9 @@ ZMK_SUBSCRIPTION(widget_modifiers, zmk_keycode_state_changed);
 int zmk_widget_modifiers_init(struct zmk_widget_modifiers *widget,
                               lv_obj_t *parent) {
   widget->obj = lv_label_create(parent);
+  // Set empty text initially to prevent "Text" from appearing
+  lv_label_set_text(widget->obj, "");
+  
   sys_slist_append(&widgets, &widget->node);
   widget_modifiers_init();
   return 0;
