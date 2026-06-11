c80432299590ced253877540022e931212b20884
Badjau <franzvasquez4153@gmail.com>
2026-06-11 16:28:34 +0800

    removed charging number

--- boards/shields/nice_oled/widgets/battery.c
+++ boards/shields/nice_oled/widgets/battery.c
@@ -78,22 +78,12 @@ static void draw_level(lv_obj_t *canvas, const struct status_state *state) {
 static void draw_charging_level(lv_obj_t *canvas, const struct status_state *state) {
     lv_draw_img_dsc_t img_dsc;
     lv_draw_img_dsc_init(&img_dsc);
-    lv_draw_label_dsc_t label_right_dsc;
-#if IS_ENABLED(CONFIG_NICE_EPAPER_ON)
-    init_label_dsc(&label_right_dsc, LVGL_FOREGROUND, &pixel_operator_mono_16, LV_TEXT_ALIGN_RIGHT);
-#else
-    init_label_dsc(&label_right_dsc, LVGL_FOREGROUND, &pixel_operator_mono_16, LV_TEXT_ALIGN_LEFT);
-#endif // CONFIG_NICE_EPAPER_ON
 
-    char text[10] = {};
-
-    sprintf(text, "%i", state->battery);
-    lv_canvas_draw_text(canvas, CONFIG_NICE_OLED_WIDGET_BATTERY_CUSTOM_X, CONFIG_NICE_OLED_WIDGET_BATTERY_CUSTOM_Y, 35, &label_right_dsc, text);
-#if IS_ENABLED(CONFIG_NICE_EPAPER_ON)
-    lv_canvas_draw_img(canvas, CONFIG_NICE_OLED_WIDGET_BATTERY_CUSTOM_X + 36, CONFIG_NICE_OLED_WIDGET_BATTERY_CUSTOM_Y + 2, &bolt, &img_dsc);
-#else
-    lv_canvas_draw_img(canvas, CONFIG_NICE_OLED_WIDGET_BATTERY_CUSTOM_X + 25, CONFIG_NICE_OLED_WIDGET_BATTERY_CUSTOM_Y, &bolt, &img_dsc);
-#endif // CONFIG_NICE_EPAPER_ON
+    // Only draw the bolt (lightning) icon, no number
+    lv_canvas_draw_img(canvas,
+        CONFIG_NICE_OLED_WIDGET_BATTERY_CUSTOM_X,
+        CONFIG_NICE_OLED_WIDGET_BATTERY_CUSTOM_Y,
+        &bolt, &img_dsc);
 }
 
 void draw_battery_status(lv_obj_t *canvas, const struct status_state *state) {
